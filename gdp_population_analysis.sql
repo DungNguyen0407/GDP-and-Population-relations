0a7f8299e703a7f1f85b326d81678dcd1f4684a2
DungNguyen0407 <minhdungnguyen0407@gmail.com>
2026-09-09 12:14:35 -0700

    gdp_population_analysis.sql

--- gdp_population_analysis.sql
+++ gdp_population_analysis.sql
@@ -1,10 +1,10 @@
 WITH Population_GDP_of_Europe_and_Asia AS (
 SELECT 
   PO.Country,
-  `2015 population`,
-  `2020 population`,
-  `2022 population`,
-  `2023 population`,
+  PO.`2015 population`,
+  PO.`2020 population`,
+  PO.`2022 population`,
+  PO.`2023 population`,
   continent AS Continent,
   GD.GDP_Full_USD AS Gross_Domestic_Product,
   GD.GDP_per_Capita_USD AS GDP_Capita
@@ -12,12 +12,13 @@ FROM `capstone-project-508019.World_population.Population_growth` AS PO
  Left JOIN `capstone-project-508019.World_population.GDP` AS GD ON PO.country = GD.Country
 WHERE   
      PO.continent in ('Europe','Asia')
-     And CAST(Replace(Replace(GD.GDP_Full_USD, '$', ''), ',', '') AS INT64) >= 200000000
-     And GD.GDP_per_Capita_USD > 3000
-     And CAST (PO.`world percentage` AS FLOAT64) != 0.0
+     AND CAST(Replace(Replace(GD.GDP_Full_USD, '$', ''), ',', '') AS INT64) >= 200000000
+     AND GD.GDP_per_Capita_USD > 3000
+     AND CAST (PO.`world percentage` AS FLOAT64) != 0.0
 )
 
 SELECT *
 FROM Population_GDP_of_Europe_and_Asia
-ORDER BY Population_GDP_of_Europe_and_Asia.Gross_Domestic_Product DESC
+ORDER BY 
+  Population_GDP_of_Europe_and_Asia.Gross_Domestic_Product DESC
 LIMIT 20
