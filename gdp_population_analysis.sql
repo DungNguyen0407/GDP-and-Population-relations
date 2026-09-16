d4446313022269f63f3a3fe1ded725b0cbc7442f
DungNguyen0407 <minhdungnguyen0407@gmail.com>
2026-09-15 20:11:37 -0700

    gdp_population_analysis.sql

--- gdp_population_analysis.sql
+++ gdp_population_analysis.sql
@@ -1,15 +1,16 @@
 WITH Population_GDP_of_Europe_and_Asia AS (
 SELECT 
   PO.Country,
-  PO.`2015 population`,
+  continent AS Continent,
   PO.`2020 population`,
   PO.`2022 population`,
   PO.`2023 population`,
-  GD.GDP_Growth_Percent,
-  continent AS Continent,
-  CAST(GD.GDP_Full_USD AS BIGNUMERIC) AS Gross_Domestic_Product,
+  PO.`2025 population`,
+  GD.GDP_Growth_Percent AS GDP_Growth_2025,
+  GD.GDP_2023_USD AS GDP_2023,
+  CAST(GD.GDP_Full_USD AS BIGNUMERIC) AS GDP_2025,
   GD.GDP_per_Capita_USD AS GDP_Capita
-FROM `capstone-project-508019.World_population.Population_growth` AS PO
+FROM `capstone-project-508019.World_population.Population` AS PO
  Left JOIN `capstone-project-508019.World_population.GDP` AS GD ON PO.country = GD.Country
 WHERE   
      PO.continent in ('Europe','Asia','North America')
@@ -21,6 +22,6 @@ WHERE
 SELECT *
 FROM Population_GDP_of_Europe_and_Asia
 ORDER BY 
-  Population_GDP_of_Europe_and_Asia.Gross_Domestic_Product DESC,
+  Population_GDP_of_Europe_and_Asia.GDP_2025 DESC,
   Country ASC
 LIMIT 25
