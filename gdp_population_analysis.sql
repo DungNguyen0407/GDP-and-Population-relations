96ec7226b5c174690e22917bf115c53fc74b44eb
DungNguyen0407 <minhdungnguyen0407@gmail.com>
2026-09-15 19:47:45 -0700

    Update gdp_population_analysis.sql

--- gdp_population_analysis.sql
+++ gdp_population_analysis.sql
@@ -5,13 +5,14 @@ SELECT
   PO.`2020 population`,
   PO.`2022 population`,
   PO.`2023 population`,
+  GD.GDP_Growth_Percent,
   continent AS Continent,
   CAST(GD.GDP_Full_USD AS BIGNUMERIC) AS Gross_Domestic_Product,
   GD.GDP_per_Capita_USD AS GDP_Capita
 FROM `capstone-project-508019.World_population.Population_growth` AS PO
  Left JOIN `capstone-project-508019.World_population.GDP` AS GD ON PO.country = GD.Country
 WHERE   
-     PO.continent in ('Europe','Asia')
+     PO.continent in ('Europe','Asia','North America')
      AND GD.GDP_Full_USD >= 200000000
      AND GD.GDP_per_Capita_USD > 3000
      AND CAST (PO.`world percentage` AS FLOAT64) != 0.0
@@ -22,4 +23,4 @@ FROM Population_GDP_of_Europe_and_Asia
 ORDER BY 
   Population_GDP_of_Europe_and_Asia.Gross_Domestic_Product DESC,
   Country ASC
-LIMIT 20
+LIMIT 25
